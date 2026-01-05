7a8cb79cd7182826443e7c610cb440b8edd1f6c6
Eltrac <hi@guhub.cn>
2026-01-05 14:20:46 +0800

    add markdown footnote support
    - `<leader>fn` to create footnote
    - `<leader>of` to organize footntoe
    - `]f` `[f` to move to next/previous footnote

---
+++ lua/plugins/footnote.lua
@@ -0,0 +1,13 @@
+return {
+  'chenxin-yan/footnote.nvim',
+  opts = {
+      keys = {
+        new_footnote = '<leader>fn',
+        organize_footnotes = '<leader>of',
+        next_footnote = ']f',
+        prev_footnote = '[f',
+      },
+      organize_on_save = false,
+      organize_on_new = false,
+  }
+}

665f1b5891e4214291102ff6dc8ec330523287a8
Eltrac <hi@guhub.cn>
2026-01-07 14:01:08 +0800

    do not load duck.nvim on startup

--- lua/plugins/duck.lua
+++ lua/plugins/duck.lua
@@ -7,6 +7,5 @@ return {
       vim.keymap.set('n', '<leader>dd', function() require("duck").hatch(symbol, speed) end, {})
       vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
       vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
-      require("duck").hatch(symbol, speed)
     end
 }

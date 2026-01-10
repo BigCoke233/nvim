d8c6e6cd0ad0759fc04cfbaad371b1611aa9e245
Eltrac <hi@guhub.cn>
2026-01-10 09:43:19 +0800

    set up treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  config = function ()
+    require("nvim-treesitter").setup()
+    require("nvim-treesitter").install({ 'javascript', 'html', 'css', 'go' })
+  end
 }

5e38e21a71f99d45f7dc7ed14e710bbb30ed1fef
Eltrac <hi@guhub.cn>
2026-01-23 10:01:43 +0800

    update footnote.nvim keymap

--- lua/plugins/footnote.lua
+++ lua/plugins/footnote.lua
@@ -2,12 +2,17 @@ return {
   'chenxin-yan/footnote.nvim',
   opts = {
       keys = {
-        new_footnote = '<leader>fn',
-        organize_footnotes = '<leader>of',
-        next_footnote = ']f',
-        prev_footnote = '[f',
+        n = {
+            new_footnote = '<leader>fn',
+            organize_footnotes = '<leader>of',
+            next_footnote = ']f',
+            prev_footnote = '[f',
+        },
+        i = { -- insert mode
+            new_footnote = '<C-f>',
+        },
       },
-      organize_on_save = false,
+      organize_on_save = true,
       organize_on_new = false,
   }
 }
